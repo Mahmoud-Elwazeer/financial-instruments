0995efc062f684fde17c3475b37176493cb57df1
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-12-18 18:32:01 +0200

    add CI/CD

--- docker/silent-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$SILENT_MODE" = "true" ]; then
-    echo "Silent mode enabled. Logs are suppressed."
-    exec nginx -g "daemon off;" > /dev/null 2>&1
-else
-    exec nginx -g "daemon off;"
-fi
